3c98489c7efdf2c66b069243c750817da7a64137
my name <qwe3546@naver.com>
2020-11-16 19:45:41 +0900

    애니메이션 방식 변경/ 엎드리기, 엎드려 이동, 위보기 구현/ 애니메이션이 계속 반복되지 않도록/프레임과 속도를 스테이트 별로 바꿔주는 함수 구현

--- 기말 프로젝트/main/player.py
+++ 기말 프로젝트/main/player.py
@@ -32,7 +32,7 @@ class Player:
     [0x88,0x89,0x8A,0xB8], # 벽잡기
     [0x70,0x71,0x72,0x73,0x74,0x75], # 공격(채찍질)
     [0x76,0x77,0x78,0x79,0x7A], # 던지기
-    [0x30,0x31,0x32,0x33,0x34,0x35,0x36], # 올려다보기
+    [0x30,0x31,0x32,0x33], # 올려다보기
     [0x20,0x21,0x22,0x23,0x24,0x25], # 점프 
     [0x26,0x27,0x28,0x29,0x2A,0x2B], # 낙하
     [0x56,0x57,0x58,0x59,0x5A,0x5B], # 밀기
@@ -51,19 +51,23 @@ class Player:
     def __init__(self):
         self.pos = 240,360
         self.dx = 0
+        self.crouch = 0
+
         self.jump_speed = 0
         self.jumpon = False
         self.jump_time = 0
+
         self.speed = 200
         self.image = gfw.image.load(gobj.RES_DIR + '/Player.png')
         self.time = 0
         self.mag = 2
-        self.FPS = 20
-        self.state = Player.IDLE
+        self.FPS = 10
+        
         self.size = 80
         self.look_left = False
 
-        self.fidx_max = False
+        self.state = Player.IDLE
+        self.fidx = 0
         
     @property
     def state(self):
@@ -73,17 +77,7 @@ class Player:
         self.__state = state
         self.anim = Player.Animation[state]
     def draw(self):
-        fidx = round(self.time * self.FPS) % len(self.anim)
-
-        if self.state in [Player.LOOKUP,Player.CROUCH, Player.FALLING] and fidx == len(self.anim) - 1:
-            self.fidx_max = True
-            fidx = len(self.anim) - 1
-        elif self.state in [Player.LOOKUP,Player.CROUCH, Player.FALLING] and self.fidx_max is True:
-            fidx = len(self.anim) - 1
-        else:
-            self.fidx_max = False
-
-        sprite_num = self.anim[fidx]
+        sprite_num = self.anim[self.fidx]
 
         sx, sy = sprite_num % 0x10, sprite_num // 0x10
         sx = sx * self.size
@@ -94,19 +88,6 @@ class Player:
         else:
             self.image.clip_composite_draw(sx, sy, self.size, self.size, 0 , 'h', *self.pos , 80,80)
 
-    def move(self):
-        if self.state is not Player.IDLE: return
-        elif self.state is Player.CROUCH:
-            self.state = Player.CROUCH_MOVE
-        else:
-            self.state = Player.MOVE
-
-    def jump(self):
-        if self.state == Player.FALLING: return
-        if self.state in [Player.IDLE,Player.MOVE]:
-            self.state = Player.JUMP
-        else:
-            pass
     def update(self):
         left,foot,right,_ = self.get_bb()                      # 바닥 체크
         tile = self.get_tile(foot)
@@ -127,13 +108,26 @@ class Player:
 
         self.wall_check(wall,left,right)
 
-        if self.state in [Player.FALLING, Player.JUMP]:
-            self.FPS = 10
         self.pos = x,y
         self.time += gfw.delta_time
 
         if self.dx is 0 and self.jump_speed is 0:
-            self.state = Player.IDLE
+            if self.crouch is 1:
+                self.state = Player.LOOKUP
+            elif self.crouch is -1:
+                self.state = Player.CROUCH
+            else:
+                self.state = Player.IDLE
+
+        if self.state in [Player.LOOKUP,Player.CROUCH, Player.FALLING, Player.JUMP] and self.fidx == len(self.anim) - 1:
+            self.fidx = len(self.anim) - 1
+        else:
+            frame = self.time * self.FPS
+            self.fidx = int(frame) % len(self.anim)
+
+        self.change_FPS()
+        self.change_speed()
+        # self.player.pos = point_add(self.player.pos, self.player.delta)
 
     def get_floor(self):
         if self.jumpon == True and self.jump_time < 0.3:
@@ -219,7 +213,11 @@ class Player:
     def handle_event(self, e):
         pair = (e.type, e.key)
         if pair in Player.KEY_MAP:
+            self.time = 0
+            if self.state is Player.CROUCH_MOVE:
+                self.fidx = 2
             self.dx += Player.KEY_MAP[pair][0]
+            self.crouch += Player.KEY_MAP[pair][1]
             if self.dx is -1:
                 self.look_left = True
                 self.move()
@@ -233,6 +231,35 @@ class Player:
                 self.jumpon = True
         elif pair == Player.KEYUP_SPACE:
             self.jumpon = False
+
+    def move(self):
+        if self.state in [Player.CROUCH, Player.CROUCH_MOVE]:
+            self.state = Player.CROUCH_MOVE
+        elif self.state in [Player.IDLE]:
+            self.state = Player.MOVE
+        else:
+            return
+
+    def jump(self):
+        if self.state == Player.FALLING: return
+        if self.state in [Player.IDLE,Player.MOVE]:
+            self.state = Player.JUMP
+        else:
+            pass
+
+    def change_FPS(self):
+        if self.state in [Player.MOVE]:
+            self.FPS = 20
+        elif self.state in [Player.LOOKUP, Player.CROUCH]:
+            self.FPS = 10
+        else:
+            self.FPS = 10
+
+    def change_speed(self):
+        if self.state in [Player.CROUCH_MOVE, Player.CROUCH]:
+            self.speed = 50
+        else:
+            self.speed = 200
 
     def get_bb(self):
         hw = 24
